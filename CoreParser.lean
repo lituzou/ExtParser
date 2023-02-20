7cc10d653911d828ca5dc1eb63e422625780b4c0
Litu Zou <litu.zou@outlook.com>
2023-02-20 15:27:18 +0000

    changed definition of wellformed grammar

--- CoreParser.lean
+++ CoreParser.lean
@@ -17,42 +17,143 @@ namespace CoreParser
     match cs with
       | [] => ε
       | c :: cs => seq (terminal c) (stringPEG cs)
-
-  def g_props (G : PEG n) (P : Fin n → (Bool × Bool × Bool)) : (Bool × Bool × Bool) :=
-    match G with
-      | ε => (false, true, false)
-      | any => (true, false, true)
-      | terminal vt => (true, false, true)
-      | nonTerminal vn => P vn
-      | seq e1 e2 => 
-        have (e1_f, e1_0, e1_s) := g_props e1 P;
-        have (e2_f, e2_0, e2_s) := g_props e2 P;
-        (
-          e1_f || ((e1_0 || e1_s) && e2_f), 
-          e1_0 && e2_0, 
-          (e1_s && (e2_0 || e2_s)) || (e1_0 && e2_s)
-        )
-      | prior e1 e2 =>
-        have (e1_f, e1_0, e1_s) := g_props e1 P;
-        have (e2_f, e2_0, e2_s) := g_props e2 P;
-        (
-          e1_f && e2_f, 
-          e1_0 || (e1_f && e2_0), 
-          e1_s || (e1_f && e2_s)
-        )
-      | star e =>
-        have (e_f, e_0, e_s) := g_props e P;
-        (false, e_f, e_s)
-      | notP e =>
-        have (e_f, e_0, e_s) := g_props e P;
-        (e_s || e_0, e_f, false)
-
-  def rho_props (vn_1 vn_2 : Fin n) (Pexp : Fin n → PEG n) (P : Fin n → (Bool × Bool × Bool)) : (Bool × Bool × Bool) :=
-    if vn_1 = vn_2 then
-      g_props (Pexp vn_2) P
-    else
-      P vn_2
   
-  def allFalse (_ : Fin n) : (Bool × Bool × Bool) := (false, false, false)
+  abbrev GProd n := Fin n → PEG n
+
+  inductive Maybe (p : α → Prop) (a : α) where
+    | found : p a → Maybe p a
+    | unknown
+
+  open Maybe
+
+  mutual
+    inductive PropF : GProd n → PEG n → Prop where
+      | any : PropF Pexp any
+      | terminal : ∀ (c : Char), PropF Pexp (terminal c)
+      | nonTerminal : ∀ (vn : Fin n), PropF Pexp (Pexp vn) → PropF Pexp (nonTerminal vn) 
+      | seq_F : ∀ (e1 e2 : PEG n), PropF Pexp e1 → PropF Pexp (seq e1 e2)
+      | seq_0F : ∀ (e1 e2 : PEG n), Prop0 Pexp e1 → PropF Pexp e2 → PropF Pexp (seq e1 e2)
+      | seq_SF : ∀ (e1 e2 : PEG n), PropS Pexp e1 → PropF Pexp e2 → PropF Pexp (seq e1 e2)
+      | prior : ∀ (e1 e2 : PEG n), PropF Pexp e1 → PropF Pexp e2 → PropF Pexp (prior e1 e2)
+      | star : ∀ (e : PEG n), PropF Pexp (star e)
+      | notP_0 : ∀ (e : PEG n), Prop0 Pexp e → PropF Pexp (notP e)
+      | notP_S : ∀ (e : PEG n), PropS Pexp e → PropF Pexp (notP e)
+
+    inductive Prop0 : GProd n → PEG n → Prop where
+      | ε : Prop0 Pexp ε
+      | nonTerminal : ∀ (vn : Fin n), Prop0 Pexp (Pexp vn) → Prop0 Pexp (nonTerminal vn) 
+      | seq : ∀ (e1 e2 : PEG n), Prop0 Pexp e1 → Prop0 Pexp e2 → Prop0 Pexp (seq e1 e2)
+      | prior_0 : ∀ (e1 e2 : PEG n), Prop0 Pexp e1 → Prop0 Pexp (prior e1 e2)
+      | prior_F0 : ∀ (e1 e2 : PEG n), PropF Pexp e1 → Prop0 Pexp e2 → Prop0 Pexp (prior e1 e2)
+      | star : ∀ (e : PEG n), PropF Pexp e → Prop0 Pexp (star e)
+      | notP : ∀ (e : PEG n), PropF Pexp e → Prop0 Pexp (notP e)
+      
+    inductive PropS : GProd n → PEG n → Prop where
+      | any : PropS Pexp any
+      | terminal : ∀ (c : Char), PropS Pexp (terminal c)
+      | nonTerminal : ∀ (vn : Fin n), PropS Pexp (Pexp vn) → PropS Pexp (nonTerminal vn) 
+      | seq_S0 : ∀ (e1 e2 : PEG n), PropS Pexp e1 → Prop0 Pexp e2 → PropS Pexp (seq e1 e2)
+      | seq_0S : ∀ (e1 e2 : PEG n), Prop0 Pexp e1 → PropS Pexp e2 → PropS Pexp (seq e1 e2)
+      | seq_SS : ∀ (e1 e2 : PEG n), PropS Pexp e1 → PropS Pexp e2 → PropS Pexp (seq e1 e2)
+      | prior_S : ∀ (e1 e2 : PEG n), PropS Pexp e1 → PropS Pexp (prior e1 e2)
+      | prior_FS : ∀ (e1 e2 : PEG n), PropF Pexp e1 → PropS Pexp e2 → PropS Pexp (prior e1 e2)
+      | star : ∀ (e : PEG n), PropS Pexp e → PropS Pexp (star e)
+  end
+
+  abbrev PropsTriple (Pexp : GProd n) (G : PEG n) := Maybe (PropF Pexp) G × Maybe (Prop0 Pexp) G × Maybe (PropS Pexp) G
+
+
+  def g_props (Pexp : GProd n) (P : (i : Fin n) → PropsTriple Pexp (Pexp i)) : (G : PEG n) → PropsTriple Pexp G
+    | ε => (unknown, unknown, unknown)
+    | any => (unknown, unknown, unknown)
+    | terminal _ => (unknown, unknown, unknown)
+    | nonTerminal vn =>
+      have (e_f, e_0, e_s) := P vn
+      (
+        match e_f with
+          | found h => found (PropF.nonTerminal vn h)
+          | unknown => unknown
+        ,
+        match e_0 with
+          | found h => found (Prop0.nonTerminal vn h)
+          | unknown => unknown
+        ,
+        match e_s with
+          | found h => found (PropS.nonTerminal vn h)
+          | unknown => unknown
+      )
+    | seq e1 e2 =>
+      have (e1_f, e1_0, e1_s) := g_props Pexp P e1;
+      have (e2_f, e2_0, e2_s) := g_props Pexp P e2;
+      (
+        match (e1_f, e1_0, e1_s, e2_f) with
+          | (found h, _, _, _) => found (PropF.seq_F e1 e2 h)
+          | (_,found h0,_,found hf) => found (PropF.seq_0F e1 e2 h0 hf)
+          | (_,_,found hs,found hf) => found (PropF.seq_SF e1 e2 hs hf)
+          | _ => unknown
+        ,
+        match (e1_0, e2_0) with
+          | (found h1, found h2) => found (Prop0.seq e1 e2 h1 h2)
+          | _ => unknown
+        ,
+        match (e1_0, e1_s, e2_0, e2_s) with 
+          | (_,found hs,found h0,_) => found (PropS.seq_S0 e1 e2 hs h0)
+          | (found h0,_,_,found hs) => found (PropS.seq_0S e1 e2 h0 hs)
+          | (_,found h1,_,found h2) => found (PropS.seq_SS e1 e2 h1 h2)
+          | _ => unknown
+      )
+    | prior e1 e2 =>
+      have (e1_f, e1_0, _) := g_props Pexp P e1;
+      have (e2_f, e2_0, _) := g_props Pexp P e2;
+      (
+        match (e1_f, e2_f) with
+          | (found h1, found h2) => found (PropF.prior e1 e2 h1 h2)
+          | _ => unknown
+        ,
+        match (e1_f, e1_0, e2_0) with
+          | (_,found h,_) => found (Prop0.prior_0 e1 e2 h)
+          | (found hf,_,found h0) => found (Prop0.prior_F0 e1 e2 hf h0)
+          | _ => unknown
+        ,
+        unknown
+      )
+    | star e =>
+      have (e_f, _, e_s) := g_props Pexp P e;
+      (
+        unknown
+        ,
+        match e_f with
+          | found h => found (Prop0.star e h)
+          | unknown => unknown
+        ,
+        match e_s with
+          | found h => found (PropS.star e h)
+          | unknown => unknown
+      )
+    | notP e =>
+      have (e_f, e_0, e_s) := g_props Pexp P e;
+      (
+        match (e_0, e_s) with
+          | (found h,_) => found (PropF.notP_0 e h)
+          | (_,found h) => found (PropF.notP_S e h)
+          | _ => unknown
+        ,
+        match e_f with
+          | found h => found (Prop0.notP e h)
+          | unknown => unknown
+        ,
+        unknown
+      )
+  
+  def g_extend (Pexp : GProd n) (P : (i : Fin n) → PropsTriple Pexp (Pexp i)) (x : Fin n) : (y : Fin n) → PropsTriple Pexp (Pexp y) :=
+    fun y =>
+      if x = y then
+        g_props Pexp P (Pexp y)
+      else
+        P y
+
+  def unknownProps (Pexp : GProd n) : (i : Fin n) → PropsTriple Pexp (Pexp i) := fun _ => (unknown, unknown, unknown)
+
+
 
 end CoreParser
