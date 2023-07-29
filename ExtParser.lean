5da607eed799ee272d87c668927b59a0e885b95e
Litu Zou <litu.zou@outlook.com>
2023-07-30 01:37:46 +0200

    added some comments on extensible grammar

--- ExtParser.lean
+++ ExtParser.lean
@@ -4,3 +4,4 @@ import ExtParser.Parsing
 
 -- This is supposed to be the part for extensible grammars and the parser.
 -- We shall work on this when the verified parser for non-extensible grammar is completed.
+-- Also we may need to consider how to generalize the ways of introducing new syntax into the program.
