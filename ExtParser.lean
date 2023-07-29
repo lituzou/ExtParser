269d703f72374841db3cc972368f8a1733a39d6e
Litu Zou <litu.zou@outlook.com>
2023-07-30 01:32:41 +0200

    added some comments on extensible grammar

--- ExtParser.lean
+++ ExtParser.lean
@@ -1,3 +1,6 @@
 import ExtParser.Grammar
 import ExtParser.AST
 import ExtParser.Parsing
+
+-- This is supposed to be the part for extensible grammars and the parser.
+-- We shall work on this when the verified parser for non-extensible grammar is completed.
